0b1fb8e30b0899f7fccb66ab11ccd844ffc7c2e4
pablomendezc <pablussz@hotmail.com>
2022-03-17 12:56:47 +0100

    ddd

--- js/calculos.js
+++ js/calculos.js
@@ -1,33 +1,61 @@
-<script>
-        // Definir variables
-        var n = 0;
 
-        // Funcion para leer numeros Reales
-        function leerNumero() {
-            num = parseInt(
-                prompt("Porfi, escribe un número")
-            );
-            return num;
-        }
+       // Definir variables
+// var n = 0;
 
-        function mostrarResultados( nf ) {
-            document.getElementById('td21').innerHTML = n;
-            document.getElementById('td22').innerHTML = factorial( nf );
-        }
+// Funcion para leer numeros Reales
+function leerNumero() {
+    num = parseInt(
+        prompt("Porfi, escribe un número")
+    );
+    return num;
+}
 
-        
-        function mostrarFactorial( nf ) {
-            document.getElementById('td21').innerHTML = n;
-            document.getElementById('td22').innerHTML = factorial( nf );
-        }
+function mostrarMultiplos(n, m) {
+    document.getElementById('td11').innerHTML = n;
+    document.getElementById('td12').innerHTML = m;
+    document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
+}
 
-        function factorial( nf ) {
-            var factorial = 1;
-            for ( i=1; i <= nf; i++ ) {
-                factorial = factorial * i;
-            }
 
-            return factorial;
-        }
+function mostrarFactorial( nf ) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial( nf );
+}
 
-    </script>
+
+function mostrarResultados(op, n1, n2 = 0) {
+    
+switch (op){
+    case 'multiplo':
+    document.getElementById('td11').innerHTML = n1;
+    document.getElementById('td12').innerHTML = n2;
+    document.getElementById('td13').innerHTML = multiplosDeHasta(n1, n2);
+    break;
+
+    default:
+}
+
+
+function multiplosDeHasta(n, m) {
+    mensaje = "Los múltiplo de " +
+        n +
+        " hasta " + m +
+        " son: ";
+
+    multiplo = 0;
+    for (i = 1; multiplo < m; i++) {
+        multiplo = n * i;
+        mensaje = mensaje + multiplo + " ";
+    }
+
+    return mensaje;
+}
+
+function factorial( nf ) {
+    var factorial = 1;
+    for ( i=1; i <= nf; i++ ) {
+        factorial = factorial * i;
+    }
+    
+    return factorial;
+}
